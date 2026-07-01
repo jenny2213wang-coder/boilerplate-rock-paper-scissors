f676ff48ac45e825154c2e0398a15e8e77a28454
jenny2213wang-coder <jenny.2213.wang@gmail.com>
2026-07-01 14:15:48 +0800

    Enhance player strategy with pattern prediction
    Refactor player function to implement pattern recognition for opponent's moves.
    
    Signed-off-by: jenny2213wang-coder <jenny.2213.wang@gmail.com>

--- RPS.py
+++ RPS.py
@@ -1,10 +1,33 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if not hasattr(player, 'pattern_dict'):
+        player.pattern_dict = {}
+        player.history = []
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if prev_play:
+        player.history.append(prev_play)
 
-    return guess
+    # 1. update
+    for n in range(3, 6):
+        if len(player.history) >= n:
+            pattern = "".join(player.history[-n:])
+            prefix = pattern[:-1]
+            last_move = pattern[-1]
+            
+            key = (n, prefix) # use (n, prefix) as key
+            if key not in player.pattern_dict:
+                player.pattern_dict[key] = {'R': 0, 'P': 0, 'S': 0}
+            player.pattern_dict[key][last_move] += 1
+
+    # 2. predict
+    for n in range(5, 2, -1): # from 5 to 3
+        prefix = "".join(player.history[-(n-1):])
+        key = (n, prefix)
+        
+        if key in player.pattern_dict:
+            counts = player.pattern_dict[key]
+            # find the most requent one
+            prediction = max(counts, key=counts.get)
+            ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+            return ideal_response[prediction]
+
+    return "R" 
